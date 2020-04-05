d612d202aada21337c99783d400ccd7a9cff5772
Hudson Holanda <hudsonholanda55@gmail.com>
2020-04-05 18:35:04 -0300

    adding archive about comparison with the strict inequality operator

---
+++ javascript-algorithms-and-data-structures/01 - basic-javascript/56 - comparison-with-the-strict-inequality-operator/index.js
@@ -0,0 +1,13 @@
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
+testStrictNotEqual(17);
+testStrictNotEqual("17");
+testStrictNotEqual(12);
+testStrictNotEqual("bob");
