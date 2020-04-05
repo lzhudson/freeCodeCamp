de14e58ab0b174782c4b498d5154847c24f5d641
Hudson Holanda <hudsonholanda55@gmail.com>
2020-04-05 18:39:54 -0300

    comparison with the greater than or equal to operator

--- javascript-algorithms-and-data-structures/01 - basic-javascript/58 - comparison-with-the-greater-than-or-equal-to-operator/index.js
+++ javascript-algorithms-and-data-structures/01 - basic-javascript/58 - comparison-with-the-greater-than-or-equal-to-operator/index.js
@@ -1,13 +1,18 @@
 function testGreaterOrEqual(val) {
-  if (val) {  // Change this line
+  if (val >= 20) {  // Change this line
     return "20 or Over";
   }
 
-  if (val) {  // Change this line
+  if (val >= 10) {  // Change this line
     return "10 or Over";
   }
 
   return "Less than 10";
 }
-
+testGreaterOrEqual(0);
+testGreaterOrEqual(9);
 testGreaterOrEqual(10);
+testGreaterOrEqual(11);
+testGreaterOrEqual(19);
+testGreaterOrEqual(100);
+testGreaterOrEqual(21);
