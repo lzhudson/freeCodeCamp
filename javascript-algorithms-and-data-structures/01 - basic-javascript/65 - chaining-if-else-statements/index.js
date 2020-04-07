c14e40c75d8d69c0e1c4b47a4df0275bec44acb3
Hudson Holanda <hudsonholanda55@gmail.com>
2020-04-06 22:57:14 -0300

    adding archive about chaining if else statements

---
+++ javascript-algorithms-and-data-structures/01 - basic-javascript/65 - chaining-if-else-statements/index.js
@@ -0,0 +1,24 @@
+function testSize(num) {
+  // Only change code below this line
+  if(num < 5) {
+    return "Tiny";
+  } else if(num < 10) {
+    return "Small";
+  } else if(num < 15) {
+    return "Medium";
+  } else if(num < 20) {
+    return "Large";
+  } else if(num >= 20) {
+    return "Huge"
+  }
+  // Only change code above this line
+}
+
+console.log(testSize(0));
+console.log(testSize(4));
+console.log(testSize(5));
+console.log(testSize(8));
+console.log(testSize(10));
+console.log(testSize(15));
+console.log(testSize(17));
+console.log(testSize(25));
