e7bac18a553d7ddbad6c9393c25658483eea5ba4
Hudson Holanda <hudsonholanda55@gmail.com>
2020-04-06 22:45:09 -0300

    adding archive about comparisons with the logical and operator

---
+++ javascript-algorithms-and-data-structures/01 - basic-javascript/61 - comparisons-with-the-logical-and-operator/index.js
@@ -0,0 +1,18 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+console.log(testLogicalAnd(0));
+console.log(testLogicalAnd(24));
+console.log(testLogicalAnd(25));
+console.log(testLogicalAnd(30));
+console.log(testLogicalAnd(50));
+console.log(testLogicalAnd(51));
+console.log(testLogicalAnd(75));
